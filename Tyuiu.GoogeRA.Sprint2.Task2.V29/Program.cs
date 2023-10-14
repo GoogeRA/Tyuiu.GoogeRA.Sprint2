fdb5635fca58bab1c98c86b72df33620136faaab
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-10-14 15:16:40 +0300

    Task2

---
+++ Tyuiu.GoogeRA.Sprint2.Task2.V29/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GoogeRA.Sprint2.Task2.V29.Lib;
+
+namespace Tyuiu.GoogeRA.Sprint2.Task2.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #2| Выполнил :  Гооге Р.A. | ПКТб-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                     *");
+            Console.WriteLine("* Задание #2                                                             *");
+            Console.WriteLine("* Вариант #29                                                            *");
+            Console.WriteLine("* Выполнил :  Гооге Роберт Aлексндрович | ПКТб-23-1                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения              *");
+            Console.WriteLine("* с клавиатуры и вычисляет находится ли точка с координатами X,Y         *");
+            Console.WriteLine("* в заштрихованной области.                                              *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Введите значение переменной y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введте значение переменной X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else 
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
